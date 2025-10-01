60d7ddda7d05ce5767d61cc5fcc95cbdb1ecc24e
Nahum <nahummejiad@gmail.com>
2025-10-01 16:40:41 -0600

    Corrección de errores en el CRUD de lugar de compras

--- src/app/modules/configuration/warehouses/create-wherehouse/create-wherehouse.component.ts
+++ src/app/modules/configuration/warehouses/create-wherehouse/create-wherehouse.component.ts
@@ -11,49 +11,62 @@ import { ToastrService } from 'ngx-toastr';
 export class CreateWherehouseComponent {
 
   @Output() WareHouseC: EventEmitter<any> = new EventEmitter();
-  @Input() SUCURSALES:any = [];
-  name:string = '';
-  address:string = '';
-  sucursale_id:string = '';
-
-  isLoading:any;
+  @Input() SUCURSALES: any = [];
+  
+  name: string = '';
+  address: string = '';
+  sucursale_id: string = '';
+  isLoading: any;
 
   constructor(
     public modal: NgbActiveModal,
     public wareHouseService: WarehouseService,
     public toast: ToastrService,
-  ) {
-    
-  }
+  ) {}
 
   ngOnInit(): void {
-    //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
-    //Add 'implements OnInit' to the class.
+    console.log('Sucursales recibidas:', this.SUCURSALES);
     
+    // Si no hay sucursales, cargarlas
+    if (!this.SUCURSALES || this.SUCURSALES.length === 0) {
+      this.loadSucursales();
+    }
   }
 
-  store(){
-    if(!this.name){
-      this.toast.error("Validación","El nombre de la sucursal es requerido");
-      return false;
+  loadSucursales() {
+    this.wareHouseService.listWarehouses(1, '').subscribe((resp: any) => {
+      this.SUCURSALES = resp.sucursales;
+      console.log('Sucursales cargadas:', this.SUCURSALES);
+    });
+  }
+
+  store() {
+    if (!this.name) {
+      this.toast.error("Validación", "El nombre del almacén es requerido");
+      return;
     }
+
+    // Hacer la sucursal opcional (quitar validación si es opcional)
+    // if (!this.sucursale_id) {
+    //   this.toast.error("Validación", "La sucursal es requerida");
+    //   return;
+    // }
 
     let data = {
       name: this.name,
-      address:this.address,
-      sucursale_id: this.sucursale_id,
-    }
+      address: this.address,
+      sucursale_id: this.sucursale_id || null,
+    };
 
-    this.wareHouseService.registerWarehouse(data).subscribe((resp:any) => {
+    this.wareHouseService.registerWarehouse(data).subscribe((resp: any) => {
       console.log(resp);
-      if(resp.message == 403){
-        this.toast.error("Validación",resp.message_text);
-      }else{
-        this.toast.success("Exito","El almacen se registro correctamente");
+      if (resp.message == 403) {
+        this.toast.error("Validación", resp.message_text);
+      } else {
+        this.toast.success("Éxito", "El almacén se registró correctamente");
         this.WareHouseC.emit(resp.warehouse);
         this.modal.close();
       }
-    })
+    });
   }
-
 }
